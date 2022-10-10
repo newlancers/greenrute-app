b55490046eda2e04e134b9685869361864709e17
Nazar Niphone <ndzobas@gmail.com>
2022-10-10 09:33:24 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/newlancers/green_rute/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.newlancers.green_rute
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      title: 'Welcome to Flutter',
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text(
+            'GreenRute',
+            style: TextStyle(
+              color: CupertinoColors.white,
+            ),
+          ),
+          backgroundColor: Color(0xff16a34a),
+          brightness: Brightness.dark,
+        ),
+        child: SafeArea(
+          child: Center(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
